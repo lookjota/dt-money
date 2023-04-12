569e6c39c2d54d2b7ca780d1ee2b7eb8a4fc682c
krasny <lookjota@gmail.com>
2023-04-12 04:43:38 -0300

    components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyles } from "./styles/global";
+import { Transactions } from "./pages/Transactions";
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles />
+      <GlobalStyles /> 
 
-      <div>hello word</div>
+      <Transactions />
+
     </ThemeProvider>
   )
 }
